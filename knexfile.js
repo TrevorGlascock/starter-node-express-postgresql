14474353ad856451c9a83b00330ff59aabedf3c6
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-08-19 16:12:58 -0500

    fixed bad connection

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ const { DATABASE_URL } = process.env;
 module.exports = {
   development: {
     client: "postgresql",
-    connection: "DATABASE_URL",
+    connection: DATABASE_URL,
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
