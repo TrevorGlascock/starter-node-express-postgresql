62aaca6f6cc018cac10ab1fc5376b46ab38e99f3
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-08-23 10:45:16 -0500

    created productExists middleware and updated Read operation

--- src/products/products.controller.js
+++ src/products/products.controller.js
@@ -1,7 +1,24 @@
 const service = require("./products.service");
 
+/**************************** Middleware Functions ****************************/
+function productExists(req, res, next) {
+  service
+    .read(req.params.productId)
+    .then((product) => {
+      if (product) {
+        res.locals.product = product;
+        return next();
+      }
+      return next({ status: 404, message: `Product cannot be found.` });
+    })
+    .catch(next);
+}
+
+/**************************** CRUDL Operations ****************************/
 function read(req, res, next) {
-  res.json({ data: { product_title: "some product title" } });
+  // Aliasing product as data
+  const { product: data } = res.locals;
+  res.json({ data });
 }
 
 function list(req, res, next) {
@@ -12,6 +29,6 @@ function list(req, res, next) {
 }
 
 module.exports = {
-  read: [read],
+  read: [productExists, read],
   list: [list],
 };
