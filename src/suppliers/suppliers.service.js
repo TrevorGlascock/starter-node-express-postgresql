25fa6f34df666a5de007365193ab9bcb85e0ebc9
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-08-23 11:49:46 -0500

    fixed incorrect use of first()

--- src/suppliers/suppliers.service.js
+++ src/suppliers/suppliers.service.js
@@ -4,11 +4,11 @@ function create(supplier) {
   return db("suppliers")
     .insert(supplier)
     .returning("*")
-    .then((rows) => rows[0]); //returning cannot use first() so it must be written out
+    .then((rows) => rows[0]); // Returning cannot use first() so it must be written out
 }
 
 function read(supplier_id) {
-  return db("suppliers").select("*").where({ supplier_id }).first();
+  return db("suppliers").select("*").where({ supplier_id }).first(); // select only version of then((rows) => rows[0]);
 }
 
 function update(newSupplier) {
@@ -16,7 +16,7 @@ function update(newSupplier) {
     .select("*")
     .where({ supplier_id: newSupplier.supplier_id })
     .update(newSupplier, "*")
-    .first(); // select only version of then((rows) => rows[0]);
+    .then((rows) => rows[0]); // update cannot use first() so it must be written out
 }
 
 module.exports = {
