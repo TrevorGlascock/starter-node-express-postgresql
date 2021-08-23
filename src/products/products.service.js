81c4c889ab03b7c372fbd074de180ecb921f8ef0
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-08-23 10:25:14 -0500

    added read operation

--- src/products/products.service.js
+++ src/products/products.service.js
@@ -4,6 +4,11 @@ function list() {
   return db("products").select("*");
 }
 
+function read(product_id) {
+  return db("products").select("*").where({ product_id }).first();
+}
+
 module.exports = {
   list,
+  read,
 };
