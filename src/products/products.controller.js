0709b3f22a2abb0237929a2af3fa27e4811e60c8
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-08-25 13:24:45 -0500

    fixed bad variable name

--- src/products/products.controller.js
+++ src/products/products.controller.js
@@ -19,7 +19,7 @@ function read(req, res, next) {
 }
 
 async function list(req, res, next) {
-  const data = await productsService.list();
+  const data = await service.list();
   res.json({ data });
 }
 
